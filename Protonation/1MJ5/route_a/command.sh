cf3f8ad64f002bdb0e22be2cc7968a40e89afa93
f.grunewald <f.grunewald@rug.nl>
2023-06-30 11:26:05 +0200

    route_a 1mj5

---
+++ Protonation/1MJ5/route_a/command.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir output
+cd output
+martinize2 -resid input -f ../1mj5_amber_named_his.pdb -x cg.pdb -o sys.top -dssp -ff martini3001 -scfix -from amber -maxwarn 1 >& command.log
